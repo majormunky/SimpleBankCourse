f6b3c3f145f986adf69677f8782145a449dafe6a
Josh Bright <josh.bright@gmail.com>
2024-11-01 21:29:51 -0700

    Added tests

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "josh",
+		Balance:  100,
+		Currency: "USD",
+	}
+	ctx := context.Background()
+
+	account, err := testQueries.CreateAccount(ctx, arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}
