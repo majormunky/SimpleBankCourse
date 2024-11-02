9611d303300887ec0ce12bce19762429c1ba8087
Josh Bright <josh.bright@gmail.com>
2024-11-01 18:05:04 -0700

    Added transfer queries

---
+++ db/query/transfer.sql
@@ -0,0 +1,42 @@
+-- name: CreateTransfer :one
+INSERT INTO transfers (
+    from_account_id,
+    to_account_id,
+    amount
+) VALUES (
+    $1, $2, $3
+)
+RETURNING *;
+
+-- name: GetTransfer :one
+SELECT *
+FROM transfers
+WHERE id = $1
+LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT *
+FROM transfers
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: ListTransfersByFromAccount :many
+SELECT *
+FROM transfers
+WHERE from_account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name: ListTransfersByToAccount :many
+SELECT *
+FROM transfers
+WHERE to_account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name DeleteTransfer :exec
+DELETE FROM transfers
+WHERE id = $1;
